eadf3d28ce65d0b81fab698f1144e379a6843139
suveiko <suveikosasha@gmail.com>
2022-09-01 17:25:34 +0400

    refactor: filter button logic

--- src/common/components/FilteredButton/FilteredButton.tsx
+++ src/common/components/FilteredButton/FilteredButton.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
@@ -12,22 +12,31 @@ export const FilteredButton: FC = () => {
   const dispatch = useAppDispatch();
 
   const userID = useAppSelector(getId);
+  const [value, setValue] = useState(false);
 
   const getMyPacks = (): void => {
     dispatch(setPacksParams({ user_id: userID }));
+    setValue(true);
   };
 
   const getAllPacks = (): void => {
     dispatch(setPacksParams({ user_id: undefined }));
+    setValue(false);
   };
 
   return (
     <ButtonGroup className={styles.filteredContainer}>
       <span>Show packs cards</span>
-      <Button onClick={getMyPacks} className={styles.itemButton}>
+      <Button
+        onClick={getMyPacks}
+        className={`${styles.itemButton} ${value ? styles.active : ''}`}
+      >
         My
       </Button>
-      <Button onClick={getAllPacks} className={styles.itemButton}>
+      <Button
+        onClick={getAllPacks}
+        className={`${styles.itemButton} ${!value ? styles.active : ''}`}
+      >
         All
       </Button>
     </ButtonGroup>
