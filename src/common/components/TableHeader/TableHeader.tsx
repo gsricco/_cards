0eff8c7ba996921690fb84824a45119ac9a684fb
SergioGord <gsricco@yandex.ru>
2022-09-01 15:14:31 +0300

    feat: add change  sortPack logic

--- src/common/components/TableHeader/TableHeader.tsx
+++ src/common/components/TableHeader/TableHeader.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
+
+import { setPacksParams } from '../../../features';
+import { useAppDispatch } from '../../../hooks';
 
 import styles from './TableHeader.module.scss';
 
@@ -19,13 +22,28 @@ export const TableHeader: FC<Props> = ({
   fourthCell,
   fifthCell,
 }) => {
+  const dispatch = useAppDispatch();
+  const [changeSortPack, setChangeSortPack] = useState(true);
+
+  const getSortPack = (): void => {
+    setChangeSortPack(!changeSortPack);
+    const sortPacks = changeSortPack ? '1updated' : '0updated';
+
+    dispatch(setPacksParams({ sortPacks }));
+  };
+
   return (
     <TableHead className={styles.tableHead}>
       <TableRow>
         <TableCell className={styles.tableFirstCell}>{firstCell}</TableCell>
         <TableCell className={styles.tableSecondCell}>{secondCell}</TableCell>
         <TableCell>
-          <TableSortLabel className={styles.tableSortLabel} active direction="desc">
+          <TableSortLabel
+            className={styles.tableSortLabel}
+            active
+            direction="desc"
+            onClick={getSortPack}
+          >
             {thirdCell}
           </TableSortLabel>
         </TableCell>
