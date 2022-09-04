9918f1df5cc4e49a0da429f54fc04ac10ff6c00d
SergioGord <gsricco@yandex.ru>
2022-09-04 12:59:52 +0300

    refactor: Grade component logic

--- src/features/cards/Cards/CardsTableBody/Card/Grade/Grade.tsx
+++ src/features/cards/Cards/CardsTableBody/Card/Grade/Grade.tsx
@@ -1,34 +1,15 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 
-import starBorder from 'assets/images/Star-border.svg';
-import starHalf from 'assets/images/Star-half.svg';
-import starFull from 'assets/images/Star.svg';
+import { Rating, Stack } from '@mui/material';
 
 type Props = {
   grade: number;
 };
 
 export const Grade: FC<Props> = ({ grade }) => {
-  const star = starBorder;
-  let stars = [star, star, star, star, star];
-  // eslint-disable-next-line no-magic-numbers
-  const sur = grade - Math.trunc(grade) >= 0.5 ? 0.5 : 0;
-
-  stars = stars.map((star, index) => {
-    if (grade >= index + 1) return starFull;
-    // eslint-disable-next-line no-magic-numbers
-    if (sur === 0.5 && Math.trunc(grade) === index) return starHalf;
-
-    return starBorder;
-  });
-
   return (
-    <div>
-      {grade}
-      {stars.map((star, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <img key={index} src={star} alt="rating" />
-      ))}
-    </div>
+    <Stack spacing={1}>
+      <Rating name="half-rating-read" defaultValue={grade} precision={0.1} readOnly />
+    </Stack>
   );
 };
