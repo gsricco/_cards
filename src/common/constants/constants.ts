df18958ad34d1b1a35ae568b0d3203e8b48af6d5
suveiko <suveikosasha@gmail.com>
2022-09-04 14:01:57 +0400

    fix: remove unused constants

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -2,5 +2,3 @@ export const MIN_PASSWORD_LENGTH = 8;
 export const DELAY_TIME = 1000;
 export const MAX_NUMBER_OF_PACKS = 110;
 export const MIN_SELECT_VALUE = 5;
-export const HALF_A_STAR = 5;
-export const ID_FOR_STAR = Math.random().toString();
