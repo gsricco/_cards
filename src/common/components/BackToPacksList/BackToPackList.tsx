a254faa0a217cdb60ddd53a12faec26ae3168510
SergioGord <gsricco@yandex.ru>
2022-09-06 23:35:26 +0300

    feat: add styles BackToPackList components

--- src/common/components/BackToPacksList/BackToPackList.tsx
+++ src/common/components/BackToPacksList/BackToPackList.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 
 import { Link } from 'react-router-dom';
 
+import styles from './BackToPackList.module.scss';
+
 import arrowImage from 'assets/images/Arrow.png';
 import { Path } from 'common';
 
 export const BackToPackList: FC = () => {
   return (
-    <div>
+    <div className={styles.arrowToBack}>
       <Link to={Path.PACKS}>
         <img alt="arrow" src={arrowImage} />
         <span> Back to Packs List</span>
